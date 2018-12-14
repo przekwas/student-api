5ef05a84a2eec58e3eecd0a824850769b3efe72c
Luke Przekwas <luke@covalence.io>
2018-12-14 16:04:39 -0600

    testing next heroku build

--- src/server/server.js
+++ src/server/server.js
@@ -7,5 +7,5 @@ app.use('/', indexRouter);
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}!`);
+    console.log(`Listening on port ${port}.`);
 });
