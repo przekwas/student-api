c8689f5bd5164e83fe4e6f302ca7b90743140421
Luke Przekwas <luke@covalence.io>
2018-12-14 16:26:30 -0600

    adding cors

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const indexRouter = require('./routes');
 
 let app = express();
+
+app.use(cors());
 
 app.use('/', indexRouter);
 
