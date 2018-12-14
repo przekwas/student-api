cd9f886887f0f1f5b78fc5cf0c00b0516f787774
Luke Przekwas <luke@covalence.io>
2018-12-14 16:28:37 -0600

    adding get one with knex

--- src/server/routes/api/hiphop.js
+++ src/server/routes/api/hiphop.js
@@ -4,13 +4,25 @@ const knex = require('../../db');
 let router = express.Router();
 
 router.get('/:id?', async (req, res, next) => {
-    try {
-        let albums = await knex.select().from('HipHop_Albums');
-        res.send(albums);
-    } catch(e) {
-        console.log(e);
-        res.statusCode(500);
+    let id = req.params.id;
+    if (id) { 
+        try {
+            let albums = await knex.select().from('HipHop_Albums').where('id', id);
+            res.send(albums);
+        } catch(e) {
+            console.log(e);
+            res.statusCode(500);
+        }
+    } else {
+        try {
+            let albums = await knex.select().from('HipHop_Albums');
+            res.send(albums);
+        } catch (e) {
+            console.log(e);
+            res.statusCode(500);
+        }
     }
+
 });
 
 module.exports = router;
